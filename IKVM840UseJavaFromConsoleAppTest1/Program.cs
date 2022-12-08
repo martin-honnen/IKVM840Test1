587a2ac479c56d719db2561c486627a3e698fabe
Martin Honnen <martin.honnen@gmx.de>
2022-12-08 15:15:41 +0100

    Output .NET version

--- IKVM840UseJavaFromConsoleAppTest1/Program.cs
+++ IKVM840UseJavaFromConsoleAppTest1/Program.cs
@@ -6,7 +6,7 @@ namespace IKVM840UseJavaFromConsoleAppTest1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World: {0}", new Test1().getJavaVersion());
+            Console.WriteLine("Hello World: .NET {0}, Java {1}", Environment.Version, new Test1().getJavaVersion());
         }
     }
 }
